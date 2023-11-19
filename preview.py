c59d52795731b38a1f3f9a9f3bf5804bf5110bec
MarginalpersonQQ <gang.terry3@gmail.com>
2023-11-19 22:42:58 +0800

    update

--- preview.py
+++ preview.py
@@ -1,6 +1,8 @@
 #創建左上角的框
 
 from setting import*
+from pygame.image import load
+from os import path 
 
 class Preview:
     def __init__(self, next_shape):
@@ -11,7 +13,8 @@ class Preview:
 
         # shapes
         self.next_shapes= next_shape
-        self.shape_surfaces = {"L": }
+        # self.shape_surfaces = {shape: load("../graphics/T.png") for shape in Tetorminos.keys()}
+        self.shape_surfaces = {shape: load(path.join("..", "graphics", f"{shape}.png")).convert_alpha() for shape in Tetorminos.keys()}
 
     def run(self):
         self.display_surface.blit(self.surface, self.rect)
