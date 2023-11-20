bbac3d0db9c0e2ac5022f8c951b626e9459306bf
MarginalpersonQQ <gang.terry3@gmail.com>
2023-11-20 19:32:18 +0800

    update

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Main:
         pygame.init() #初始化
         self.Dispay_Surface = pygame.display.set_mode((Window_Width, Window_Height)) #執行遊戲外框
         self.clock = pygame.time.Clock() #遊戲時刻
+
         pygame.display.set_caption("Tetris") #設定視窗標題名稱
 
         #shapes
@@ -40,6 +41,7 @@ class Main:
     def run(self):
         while(True):
             for event in pygame.event.get(): #pygame.event.get() 取得當前發生的事件
+
                 if event.type == pygame.QUIT: #當遊戲狀態為停止時 離開遊戲
                     pygame.quit() #exit everything 
                     exit()
@@ -51,7 +53,7 @@ class Main:
             self.preview.run(self.next_shapes)
             #updatating the game
             pygame.display.update()
-            self.clock.tick() #控制遊戲幀數
+            self.clock.tick(60) #控制遊戲幀數
 
 if __name__ == "__main__":
     main = Main()
