52f8ee9f1d2f9cd7ffc0eb135f9617861be68172
MarginalpersonQQ <gang.terry3@gmail.com>
2023-11-20 12:55:04 +0800

    update

--- score.py
+++ score.py
@@ -1,11 +1,14 @@
 # 創建右下角的分數框
 from setting import*
-
+from os.path import join
 class Score:
     def __init__(self) -> None:
         self.surface = pygame.Surface((SideBar_Width, Game_Height * Score_Height_Fraction - Padding))
         self.rect = self.surface.get_rect(bottomright = (Window_Width - Padding, Window_Height - Padding))
         self.display_surface = pygame.display.get_surface()
+
+        #font 
+        self.font = pygame.font.Font(join(".", "graphic", "Russo_One.ttf"), 30)#(font, size)
 
     def run(self):
         self.display_surface.blit(self.surface, self.rect)
