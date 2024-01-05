00bf289771cc8357ca2ea09473610f0d8bafb83e
Whitebear0920 <whiteb0920@gmail.com>
2024-01-06 00:44:18 +0800

    fixed

--- sever.py
+++ sever.py
@@ -30,7 +30,7 @@ while True:
                 print("players enought")
                 print(players)
                 for j in  range(1,4): #開始倒數
-                    data = data = {
+                    data = {
                     "type" : "Time",
                     "value" : j
                     }
